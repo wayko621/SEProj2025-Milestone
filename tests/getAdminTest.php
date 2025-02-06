63c2768ef4d8014df733e96dd2972d3673fb0ad0
wayko621 <wayko@users.noreply.github.com>
2025-02-06 08:46:28 -0500

    created testGetAdmin and testGetAdmin
    PHPUnit test to see if we can get admin member

---
+++ tests/getAdminTest.php
@@ -0,0 +1,40 @@
+<?php
+namespace Devdogz\Se2015projTest;
+
+use PHPUnit\Framework\TestCase;
+
+final class getAdminTest extends TestCase
+{
+    private $mysqli;
+    private $adminRepository;
+
+    protected function setUp(): void
+    {
+        // Connect to mysqli
+        $dbConfig = new DBConfig();
+        $this->mysqli = $dbConfig->getConnection();
+        
+        // Initialize getAdmin with the database connection
+        $this->adminRepository = new getAdmin();
+    }
+
+    public function testGetUserByCredentials()
+    {
+        $firstname = 'Jose';
+        $email = "ortizj41@montclair.edu";
+
+        // Pass user's login info by FirstName and AdminID
+        $adminuser = $this->adminRepository->getUserByCredentials($firstname);
+
+        // Verify if user exist
+        $this->assertNotNull($adminuser, 'User should not be null');  // Ensure the user is found
+        $this->assertEquals('Jose', $adminuser['FirstName'], 'Firstname does not match');  
+        $this->assertEquals("ortizj41@montclair.edu", $adminuser['Email'], 'Email does not match');
+    }
+
+    protected function tearDown(): void
+    {
+        // Close the mysqli connection
+        $this->mysqli->close();
+    }
+}
