ca64921f9e297a37106371ddd55d9d89fd8f5ac2
wayko621 <wayko@users.noreply.github.com>
2025-02-13 10:33:10 -0500

    Clean up code
    Refactoring code to remove repetitive code

--- PHPFiles/faculty.php
+++ PHPFiles/faculty.php
@@ -11,19 +11,7 @@
         echo "<div style='padding-left: 10px;'>";
         echo "Name: " . htmlspecialchars($_SESSION['facUN']) . "<br/>";
         echo "Email: " . htmlspecialchars($_SESSION['facEmail']) . "<br/>";
-        echo "<button class='logout btn btn-primary btn-sm pull-left'>Log Out</button>";
-        echo "</div>"; 
-        echo "<script type='text/javascript' src='https://code.jquery.com/jquery-1.7.min.js'></script>";
-        echo "<script type='text/javascript' src='../files/js/jquery-ui-1.8.22.custom.min.js'></script>";
-        echo "<script>";
-        echo "$(document).ready(function()";
-        echo "{";
-        echo "$('.logout').on('click',function()";
-        echo "{";
-        echo "$(location).prop('href', 'sessionDestroy.php')";
-        echo "});";
-        echo "});";
-        echo " </script>"; 
+        require 'logout.php';
 
     }
 
