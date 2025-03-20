2a56d09922b4cf68d77a04d110a6270e6feb5b63
silvab11 <silvab11@montclair.edu>
2025-03-20 14:41:22 -0400

    Update getFacultyTest.php

--- tests/getFacultyTest.php
+++ tests/getFacultyTest.php
@@ -1,1 +1,40 @@
+<?php
+namespace Devdogz\Se2015projTest;
 
+use PHPUnit\Framework\TestCase;
+
+final class getFacultyTest extends TestCase
+{
+    private $mysqli;
+    private $facultyRepository;
+
+    protected function setUp(): void
+    {
+        // Connect to mysqli
+        $dbConfig = new DBConfig();
+        $this->mysqli = $dbConfig->getConnection();
+        
+        // Initialize getAdmin with the database connection
+        $this->facultyRepository = new getFaculty();
+    }
+
+    public function testGetUserByCredentials()
+    {
+        $firstname = 'Jose';
+        $email = "ortizj41@montclair.edu";
+
+        // Pass user's login info by FirstName and AdminID
+        $facultyuser = $this->facultyRepository->getUserByCredentials($firstname);
+
+        // Verify if user exist
+        $this->assertNotNull($facultyuser, 'User should not be null');  // Ensure the user is found
+        $this->assertEquals('Jose', $facultyuser['FirstName'], 'Firstname does not match');  
+        $this->assertEquals("ortizj41@montclair.edu", $facultyuser['Email'], 'Email does not match');
+    }
+
+    protected function tearDown(): void
+    {
+        // Close the mysqli connection
+        $this->mysqli->close();
+    }
+}
