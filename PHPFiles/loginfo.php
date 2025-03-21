ea0fd329fcd8464425bf80c1e61c723c29b23310
wayko621 <wayko@users.noreply.github.com>
2025-03-21 12:56:38 -0400

    fixed css for loginfo.php

--- PHPFiles/loginfo.php
+++ PHPFiles/loginfo.php
@@ -9,7 +9,7 @@
     else
     {
         session_regenerate_id(true);
-		echo "<div style='padding-left: 10px;'>"; 
+		echo "<div style='padding-left: 10px;' class='loginsection'>"; 
 		echo "Name: " . htmlspecialchars($_SESSION['adminUN']) . "<br/>";
 		echo "Email: " . htmlspecialchars($_SESSION['Email']);
 	}
