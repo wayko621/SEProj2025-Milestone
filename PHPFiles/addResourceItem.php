df8f7936dfdd3ee5082b4cf71a1564108e12e0e7
Jose Ortiz <dreamcpu@hotmail.com>
2025-03-26 09:31:11 -0400

    Update addResourceItem.php

--- PHPFiles/addResourceItem.php
+++ PHPFiles/addResourceItem.php
@@ -9,7 +9,41 @@ session_start();
     {
 
     $resourceName = $_POST['resourceName'];
-        echo($resourceName);
+
+     require 'dbconfig.php';
+        if ($resourceName == null )
+        {
+            header("refresh:2; url=addResource.php");
+            echo "Admin ID, First Name, Last Name or email was not supplied";
+        }
+        else
+        {
+            $sql1 = "Select * from resourcelist where resourceName ='" . $resourceName . "'";
+            $results = $con->query($sql1);
+
+            if($results->num_rows > 0)
+            {
+                //If email already exist user is not created"//
+                header("refresh:2; url=addResource.php");
+                echo "User Already Exist";
+            }
+            else
+            {   
+                $sql = "Insert into resourcelist(resourceName) VALUES('".$resourceName."')";
+                if($con->query($sql) === TRUE)
+                {
+                    header("refresh:2; url=addResource.php");
+                    echo "Added admin user";
+                }
+                else
+                {
+                    header("refresh:2; url=addResource.php");
+                    echo "Error: " . $sql . "<br>" . $con->error;
+                }
+            }
+        }
+
+        $con->close();
     }
 
     
