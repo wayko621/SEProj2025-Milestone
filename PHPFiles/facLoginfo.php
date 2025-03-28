09eb7ce8320803a5f7e5e6ffef7381ca973239d6
wayko621 <wayko@users.noreply.github.com>
2025-03-28 11:19:59 -0400

    move old files and fixed session

--- PHPFiles/facLoginfo.php
+++ PHPFiles/facLoginfo.php
@@ -1,6 +1,4 @@
 <?php
-
-
     if(!isset($_SESSION['loggedin']) || !isset($_SESSION['facUN']))
     {
         header("location:/SEProj2025-Milestone/");
@@ -8,6 +6,7 @@
     }
     else
     {
+        session_regenerate_id(true);
 		echo "<div style='padding-left: 10px;'>"; 
 		echo "Name: " . htmlspecialchars($_SESSION['facUN']) . "<br/>";
 		echo "Email: " . htmlspecialchars($_SESSION['facEmail']);
--- PHPFiles/loginfo.php
+++ PHPFiles/loginfo.php
@@ -1,6 +1,4 @@
 <?php
-
-
     if(!isset($_SESSION['loggedin']) || !isset($_SESSION['adminUN']))
     {
         header("location:/SEProj2025-Milestone/");
